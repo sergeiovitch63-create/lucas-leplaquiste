064d92c4d714d279c6864dc00717023728629c46
sergeiovitch63-create <sergeiovitch63@gmail.com>
2026-02-04 19:43:20 +0200

    fix: og image route jsx

---
+++ src/app/og/route.tsx
@@ -0,0 +1,31 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export function GET() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#0b2a4a",
+          color: "white",
+          fontSize: 64,
+          fontWeight: 700,
+          fontFamily: "sans-serif",
+        }}
+      >
+        Lucas le Plaquiste
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+}
+
